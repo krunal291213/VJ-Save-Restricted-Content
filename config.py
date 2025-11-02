c6664fd2513d14c0e6d604bcf0d07dac5e1e5202
krunal291213 <patelkrunal291213@gmail.com>
2025-11-02 23:56:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ CHANNEL_ID = os.environ.get("CHANNEL_ID", "")
 
 # Your Mongodb Database Url
 # Warning - Give Db uri in deploy server environment variable, don't give in repo.
-DB_URI = os.environ.get("DB_URI", "") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://patelkrunal291213:krunal@2912@cluster0.d0hhj3y.mongodb.net/?appName=Cluster0") # Warning - Give Db uri in deploy server environment variable, don't give in repo.
 DB_NAME = os.environ.get("DB_NAME", "vjsavecontentbot")
 
 # Increase time as much as possible to avoid floodwait, spamming and tg account ban issues.
